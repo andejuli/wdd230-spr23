f1104bc5fd825025b93954f1b8effb4767726785
andejuli <techtrek@hotmail.com>
2023-01-06 13:33:55 -0700

    started by portal page

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('.year').textContent = year;
+
+let currentdate = document.lastModified;
+
+document.querySelector('.updated').textContent = currentdate;
+
+
